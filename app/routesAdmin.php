46e207ba9818acc387e83e799c3caca31501c297
LAPTOP-9H3DFM2K\nbmai <hhau050501@gmail.com>
2023-03-30 00:21:20 +0700

    add route admin

---
+++ app/routesAdmin.php
@@ -0,0 +1,63 @@
+<?php
+$routes = [
+  '/' => [
+    'controller' => 'AccountController',
+    'action' => 'loginAdmin'
+  ],
+  'index' => [
+    'controller' => 'HomeController',
+    'action' => 'indexAdmin'
+  ],
+  'brand' => [
+    'controller' => 'BrandController',
+    'action' => 'index'
+  ],
+  'danh-sach-brand' => [
+    'controller' => 'BrandController',
+    'action' => 'showBrand'
+  ],
+  'create-brand' => [
+    'controller' => 'BrandController',
+    'action' => 'createBrand'
+  ],
+  'delete-brand' => [
+    'controller' => 'BrandController',
+    'action' => 'deleteBrand'
+  ],
+  'color' => [
+    'controller' => 'ColorController',
+    'action' => 'index'
+  ],
+  'danh-sach-color' => [
+    'controller' => 'ColorController',
+    'action' => 'showColor'
+  ],
+  'create-color' => [
+    'controller' => 'ColorController',
+    'action' => 'createColor'
+  ],
+  'delete-color' => [
+    'controller' => 'ColorController',
+    'action' => 'deleteColor'
+  ],
+  'size' => [
+    'controller' => 'SizeController',
+    'action' => 'index'
+  ],
+  'danh-sach-size' => [
+    'controller' => 'SizeController',
+    'action' => 'showSize'
+  ],
+  'create-size' => [
+    'controller' => 'SizeController',
+    'action' => 'createSize'
+  ],
+  'delete-size' => [
+    'controller' => 'SizeController',
+    'action' => 'deleteSize'
+  ],
+  'danh-sach-product' => [
+    'controller' => 'ProductController',
+    'action' => 'showProduct'
+  ]
+];
